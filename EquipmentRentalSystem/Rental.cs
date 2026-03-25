90c313cde104268559c33da887b0eeaeffef5443
Kacper <kacper.piotrowski1711@gmail.com>
2026-03-25 18:56:06 +0100

    Added Rental class

---
+++ EquipmentRentalSystem/Rental.cs
@@ -0,0 +1,19 @@
+﻿namespace EquipmentRentalSystem;
+
+public class Rental
+{
+    public DateOnly RentalDate { get; }
+    public DateOnly SetReturnDate { get; }
+    public DateOnly? ActualReturnDate { get; set; }
+    public User RentalUser { get; }
+    public Equipment RentedEquipment { get; }
+    public double Fees { get; set; }
+    
+    public Rental(DateOnly rentalDate,  DateOnly setReturnDate, User user, Equipment rentalEquipment)
+    {
+        RentalDate = rentalDate;
+        SetReturnDate = setReturnDate;
+        RentalUser = user;
+        RentedEquipment = rentalEquipment;
+    }
+}
